a4330184f2d2042e9db660ab0391613a128ba846
lichunhui <chunhui.li@megatronix.co>
2020-04-21 19:19:43 +0800

    add 第三方登录

--- routes/api.php
+++ routes/api.php
@@ -39,6 +39,10 @@ Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function() {
         Route::post('verificationCodes', 'VerificationCodesController@store');
         // 用户注册
         Route::post('users','UsersController@store');
+        // 第三方登录
+        Route::post('socials/{social_type}/authorizations', 'AuthorizationsController@socialStore')
+            ->where('social_type', 'weixin')
+            ->name('socials.authorizations.store');
 //    });
 
     Route::middleware('throttle:'.config('api.rate_limit.access'))->group(function() {
