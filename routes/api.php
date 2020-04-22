f5d87da287d60f848306b4ddb295478a7735a606
lichunhui <chunhui.li@megatronix.co>
2020-04-22 16:46:08 +0800

    add 用户信息

--- routes/api.php
+++ routes/api.php
@@ -47,6 +47,17 @@ Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function() {
         Route::put('authorizations/current', 'AuthorizationsController@update');
         // 删除token
         Route::delete('authorizations/current', 'AuthorizationsController@destroy');
+
+        // 游客可以访问的接口
+
+        // 某个用户的详情
+        Route::get('users/{user}', 'UsersController@show');
+
+        // 登录后可以访问的接口
+        Route::middleware('auth:api')->group(function() {
+            // 当前登录用户信息
+            Route::get('user', 'UsersController@me');
+        });
 //    });
 
     Route::middleware('throttle:'.config('api.rate_limit.access'))->group(function() {
