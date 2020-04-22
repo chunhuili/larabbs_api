edbef83e01efaae209273bc58cd615f0d940a2dc
lichunhui <chunhui.li@megatronix.co>
2020-04-22 17:51:02 +0800

    add 编辑用户信息

--- routes/api.php
+++ routes/api.php
@@ -57,6 +57,10 @@ Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function() {
         Route::middleware('auth:api')->group(function() {
             // 当前登录用户信息
             Route::get('user', 'UsersController@me');
+            // 编辑登录用户信息
+            Route::patch('user', 'UsersController@update');
+            // 上传图片
+            Route::post('images', 'ImagesController@store');
         });
 //    });
 
